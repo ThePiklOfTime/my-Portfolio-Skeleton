073518c150ed94e9b032e9b58e62b6b688890a39
nico <nicotolvanen@gmail.com>
2023-04-26 09:24:32 +0300

    started workin on mobile menu. still somewhat broken

--- src/lib/navI.svelte
+++ src/lib/navI.svelte
@@ -3,11 +3,20 @@
 	let navClass = 'list-nav';
 	let liClass = 'gap-10';
 	import { page } from '$app/stores';
+	import { drawerStore } from '@skeletonlabs/skeleton';
 	if (isRow) {
 		navClass = 'list-nav max-sm:hidden';
 		liClass = 'flex flex-row gap-10';
 	}
 	$: Active = (href: string) => (href === $page.url.pathname ? '!bg-primary-500' : '');
+	$: {
+		console.log($page.url.pathname)
+	}
+	function onClick(){
+		drawerStore.close()
+		drawerStore.update()
+	}
+	console.log("Hello world")
 </script>
 
 <nav class={navClass}>
@@ -15,12 +24,12 @@
 	<ul>
 		<li class={liClass}>
 			<span>
-				<a href="/" class={Active('/')}>
+				<a href="/" class={Active('/')} on:click={onClick}>
 					<span>Home</span>
 				</a>
 			</span>
 			<span>
-				<a href="/about" class={Active('/about')}>
+				<a href="/about" class={Active('/about')} on:click={onClick}>
 					<span>About</span>
 				</a>
 			</span>
